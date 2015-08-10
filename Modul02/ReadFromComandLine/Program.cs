d9a4127d6976fe08663095fc2eff87a2dcbe90ed
Sardelia <sali46@abv.bg>
2015-08-10 11:53:46 +0300

    Модул 02, видео 6 - Работа със стрингове

--- Modul02/ReadFromComandLine/Program.cs
+++ Modul02/ReadFromComandLine/Program.cs
@@ -15,6 +15,18 @@ namespace ReadFromComandLine
 
 			//Печат на резултат
 			Console.WriteLine ("Резултата а + в е  " +(a + b).ToString() + "\n\n\n");
+			string test = "1";
+			test += ", 2";
+			test += ", 3";
+			test += ", 4";
+			test += ", 5";
+			Console.WriteLine ("Събиране с += : " + test + "\n");
+
+			test = test.Replace (", ","; " );
+			Console.WriteLine ("Работа с Replace: " + test + "\n");
+			//ще видим кой е третия знак от масива, нарича се парсване а командата е сплит
+			Console.WriteLine ("Третия знак е: " + test.Split(';')[2]);
+
 		}
 	}
 }
